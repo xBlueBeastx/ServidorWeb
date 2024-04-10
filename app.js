e83ee80348c728abb029de2a896212d0a500e7b4
Ángel Almeida <almaworks70@gmail.com>
2024-04-09 22:07:44 -0400

    Agrega archivos de mi proyecto

---
+++ app.js
@@ -0,0 +1,5 @@
+var botonMasInformacion = document.getElementById("masInformacion")
+
+botonMasInformacion.addEventListener("click", function() {
+    alert("Esta es una práctica de Fundamentos de Criptografía con el maestro Manuel K. Guerrero, consiste en instalar un certificado SSL/TLS en un servidor web. ^_~ ");
+});
